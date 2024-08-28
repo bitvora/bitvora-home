b223acbfca8da95bf4b52df42c1453ca752e9252
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2024-08-28 23:32:40 +0100

    Add scroll indicator for pricing section

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const items = document.querySelectorAll('.why-item');
-  const dots = document.querySelectorAll('.dot');
+  function setupObserver(sectionId, dotsId) {
+    const container = document.querySelector(`#${sectionId}`);
+    const items = container.querySelectorAll('.scroll-item');
+    const dots = document.querySelectorAll(`#${dotsId} .dot`);
 
-  const observerOptions = {
-    root: document.querySelector('#why-container'),
-    rootMargin: '0px',
-    threshold: 0.5
-  };
+    const observerOptions = {
+      root: container,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      const index = entry.target.getAttribute('data-index');
-      if (entry.isIntersecting) {
-        dots.forEach((dot) => dot.classList.remove('active'));
-        document.querySelector(`.dot[data-dot="${index}"]`).classList.add('active');
-      }
-    });
-  }, observerOptions);
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const index = entry.target.getAttribute('data-index');
+        if (entry.isIntersecting) {
+          dots.forEach((dot) => dot.classList.remove('active'));
+          document.querySelector(`#${dotsId} .dot[data-dot="${index}"]`).classList.add('active');
+        }
+      });
+    }, observerOptions);
 
-  items.forEach((item) => observer.observe(item));
+    items.forEach((item) => observer.observe(item));
+  }
+
+  setupObserver('section-1', 'dots-1');
+  setupObserver('section-2', 'dots-2');
 });
