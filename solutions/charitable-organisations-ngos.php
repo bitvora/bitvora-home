bbc63d8dc91535348dea192d6a6ff1f6274ad9cd
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2024-11-29 00:01:20 +0100

    Finish charitable page

--- solutions/charitable-organisations-ngos.php
+++ solutions/charitable-organisations-ngos.php
@@ -1,178 +1,156 @@
 <html lang="en">
 
 <head>
-  <?php 
-  $pageTitle = "Charitable Organizations & NGOs";
-  $pageURL = "https://signet.bitvora.com/solutions/charitable-organisations-ngos";
-  $pageImage = "https://signet.bitvora.com/assets/bitvora.png";
+    <?php 
+   $pageTitle = "Charitable Organizations & NGOs";
+   $pageURL = "https://signet.bitvora.com/solutions/charitable-organisations-ngos";
+   $pageImage = "https://signet.bitvora.com/assets/bitvora.png";
   
   include('../includes/header.php'); ?>
 </head>
 
 <body>
-<?php include('../includes/navbar.php'); ?>
+    <?php include('../includes/navbar.php'); ?>
 
-    <main class="mt-12 pt-12 sm:mt-8 sm:pt-8 md:mt-2 lg:mt-4 xl:mt-4 2xl:mt-4 w-full mb-8 pb-8">
-        <section class="flex justify-between items-center px-6 py-5 container mx-auto w-full">
-            <div class="relative block lg:flex w-full">
+    <main class="w-full mb-8 pb-8">
+        <section class="justify-between items-center lg:px-6 mb-6 lg:mb-2 container mx-auto w-full">
+            <div class="relative w-full justify-center text-center flex flex-col-reverse lg:flex-col">
+                <div
+                    class="relative w-full h-full sm:h-[300px] lg:h-[400px] flex justify-end sm:justify-center lg:justify-end lg:py-12 mt-12 py-1 lg:mt-0">
+                    <img src="/assets/ngo.svg"
+                        class="w-full h-full object-contain solutions-header-image hidden md:flex" />
+                    <img src="/assets/ngo-mobile.svg" class="w-full h-full object-fill md:hidden" />
+                </div>
+
                 <div id="hero-section"
-                    class="w-full relative md:w-[600px] md:mx-auto lg:mx-[unset] lg:w-[600px] xl:w-[600px] 2xl:w-[600px] lg:max-w-[40%] xl:max-w-[40%] 2xl:max-w-[40%] mt-4 sm:mt-6 md:mt-6 lg:mt-8 xl:mt-8 2xl:mt-8 pt-4 sm:pt-[40px] md:pt-[50px] lg:pt-[70px] xl:pt-[70px] 2xl:pt-[70px]">
+                    class="w-full px-6 md:w-9/12 lg:w-10/12 xl:w-8/12 relative mx-auto mt-[150px] sm:mt-[120px] md:mt-[140px] lg:mt-2 xl:mt-2 2xl:mt-2 pt-2 sm:pt-[40px] md:pt-2 lg:pt-2 xl:pt-2 2xl:pt-2 justify-center text-center">
                     <h1
-                        class="text-white text-3xl z-[1] sm:text-4xl md:text-3xl lg:text-4xl 2xl:text-5xl uppercase mb-4 pb-4 tracking-normal leading-tight text-center lg:text-left font-medium">
-                        Bitcoin Payments for NGOs & Charitable Organizations
+                        class="text-white text-4xl mx-auto justify-center z-[1] sm:text-4xl md:text-3xl lg:text-4xl 2xl:text-5xl uppercase mb-4 pb-4 tracking-normal leading-tight text-center font-medium lg:w-2/3">
+                        Bitcoin Payments for
+                        Charities & NGOs
                     </h1>
 
                     <p
-                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left sm:font-medium">
-                        Offer seamless, secure, and instant Bitcoin payments for your gambling and gaming platforms,
-                        creating a smooth, frustration-free experience for players and operators.
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center sm:font-medium justify-center">
+                        Do business with clients from anywhere in the world at a fraction of the cost of traditional
+                        payment processors. Start growing your freelance business powered by the Bitcoin and Lightning
+                        networks today.
                     </p>
 
                     <a href="https://console.signet.bitvora.com/auth/signup"
-                        class="bg-primary text-white font-bold px-7 py-4 lg:py-[18px] rounded-md text-[14px] lg:text-[17px] mt-5 hover:bg-darkBg w-full lg:w-[300px] flex text-center justify-center">Explore
-                        Bitcoin Payments</a>
-                </div>
-
-                <div
-                    class="relative w-full lg:w-[60%] xl:w-[60%] 2xl:w-[60%] mt-10 flex justify-end sm:justify-center lg:justify-end py-12">
-                    <img src="/assets/ngo.png"
-                        class="w-full sm:w-[80%] md:w-[70%] lg:w-[80%] max-w-none mx-auto" />
+                        class="bg-primary text-white font-bold px-2 py-3 lg:py-4 rounded-md text-[14px] lg:text-[17px] mt-5 hover:bg-darkBg w-full md:w-[200px] lg:w-[220px] flex text-center justify-center mx-auto">
+                        Get Started Today</a>
                 </div>
             </div>
         </section>
 
         <section
-            class="w-full lg:w-8/12 justify-center items-center px-6 py-5 container mx-auto text-center mt-[15px] sm:mt-[5px] md:-mt-[25px] lg:mt-[75px] xl:mt-[75px] 2xl:mt-[75px] pt-[15px] sm:pt-[5px] md:pt-[75px] lg:pt-[75px] xl:pt-[75px] 2xl:pt-[75px] mb-8 pb-8">
+            class="w-full px-6 py-5 container mx-auto mt-[90px] sm:mt-[80px] md:-mt-[45px] lg:mt-[85px] xl:mt-[85px] 2xl:mt-[85px] pt-[15px] sm:pt-[5px] md:pt-[85px] lg:pt-[85px] xl:pt-[85px] 2xl:pt-[85px] mb-8 pb-8">
             <h4
-                class="text-white text-xl sm:text-2xl 2xl:text-3xl font-normal uppercase mb-4 sm:mb-6 md:mb-8 pb-4 sm:pb-6 md:pb-8 tracking-normal leading-tight">
-                Improved the Payment Experience
+                class="text-white text-2xl sm:text-2xl 2xl:text-3xl font-normal uppercase mb-12 sm:mb-6 md:mb-8 pb-12 sm:pb-6 md:pb-8 tracking-normal leading-tight text-center justify-center md:w-8/12 lg:w-6/12 mx-auto">
+                How Bitcoin Changes Payments
+                for charities & NGO
             </h4>
 
-            <p
-                class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-center sm:font-medium">
-                Bitcoin payments are a natural fit for online casinos, offering several key competitive advantages:
-                simplifying deposits and withdrawals, accelerating transactions, and significantly improving security.
-                With the addition of the Lightning Network, casinos can further boost transaction speed and efficiency.
-                Bitcoin allows casinos to provide players with a streamlined, efficient payment experience, all while
-                lowering operational expenses.
-            </p>
-        </section>
-
-        <section
-            class="w-full px-6 py-5 container mx-auto mt-[15px] sm:mt-[5px] md:-mt-[25px] lg:mt-[75px] xl:mt-[75px] 2xl:mt-[75px] pt-[15px] sm:pt-[5px] md:pt-[75px] lg:pt-[75px] xl:pt-[75px] 2xl:pt-[75px] mb-8 pb-8">
-            <div class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
-                <div class="col-span-3 w-full">
+            <div
+                class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12  mb-6 pb-6">
+                <div class="col-span-3 w-full px-2 lg:px-1">
                     <h4
-                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-normal text-center lg:text-left uppercase mb-4 pb-4 tracking-normal leading-tight">
-                        Instant Settlement
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Micro Donations
                     </h4>
 
                     <p
                         class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
-                        Enable your players to dive into the action immediately with instant Bitcoin settlement for both
-                        deposits and withdrawals, reducing waiting times and increasing satisfaction. The Bitcoin
-                        Lightning
-                        Network enables near real-time transactions, ensuring players can start playing instantly and
-                        receive
-                        their winnings with no delays.
+                        Micropayments aren’t possible on traditional payment processors because of the high base fee.
+                        The bitcoin lightning network allows you to send payments less than $0.01, enabling you to
+                        collect donations of any amount.
                     </p>
                 </div>
 
-                <div class="col-span-2 w-full flex justify-center items-center"">
-                    <img src="/assets/time.svg" class="solutions-image" />
+                <div class="col-span-2 w-full flex justify-center items-center mb-2 pb-2 lg:mb-0 lg:pb-0">
+                    <img src="/assets/donations-right.svg" class="solutions-image hidden md:flex" />
+                    <img src="/assets/donations-mobile.svg" class="solutions-image md:hidden" />
+                </div>
+            </div>
+
+            <div
+                class="flex flex-col lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12 lg:mt-12 lg:pt-12 mb-6 pb-6">
+                <div class="col-span-2 w-full flex justify-center items-center mb-2 pb-2 lg:mb-0 lg:pb-0">
+                    <img src=" /assets/lock.svg" class="solutions-image hidden md:flex" />
+                    <img src=" /assets/security-mobile.svg" class="solutions-image md:hidden" />
+                </div>
+
+                <div class="col-span-3 w-full px-2 lg:px-1">
+                    <h4
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Improved Security and Fraud Prevention
+                    </h4>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                        Unlike credit cards which are rife with fraud from stolen numbers and carding techniques,
+                        payments made with bitcoin can only be made by the bearer and are irreversible. Say goodbye to
+                        chargebacks and dispute fees with instant settlement, only available with Bitcoin and the
+                        Lightning network.
+                    </p>
+                </div>
+            </div>
+
+            <div
+                class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12 mb-6 pb-6">
+                <div class="col-span-3 w-full px-2 lg:px-1">
+                    <h4
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Reach New Audiences
+                    </h4>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                        Bitcoin advocates enthusiastically support charities that accept bitcoin as a form of payment.
+                        Enjoy the benefits of free marketing for your charity or NGO simply by accepting a superior form
+                        of payment. Get listed on many popular websites that showcase organizations that are joining the
+                        bitcoin ecosystem.
+                    </p>
+                </div>
+
+                <div class="col-span-2 w-full flex justify-center items-center">
+                    <img src=" /assets/hand-shake.svg" class="solutions-image hidden md:flex" />
+                    <img src="/assets/hand-shake-mobile.svg" class="solutions-image md:hidden" />
                 </div>
             </div>
 
             <div class="flex flex-col lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
                 <div class="col-span-2 w-full flex justify-center items-center"">
-                    <img src="/assets/lock.svg" class="solutions-image" />
-                </div>
-
-                <div class="col-span-3 w-full">
-                    <h4
-                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-normal text-center lg:text-left uppercase mb-4 pb-4 tracking-normal leading-tight">
-                        Resistant to Fraud
-                    </h4>
-
-                    <p
-                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
-                        Bitcoin payments are irreversible, meaning your casino is protected from fraudulent chargebacks.
-                        This reduction in operational risks helps prevent costly disputes, ensuring peace of mind and a
-                        smoother experience for operators and players alike.
-                    </p>
-                </div>
+                    <img src=" /assets/rates-left.svg" class="solutions-image hidden md:flex" />
+                <img src="/assets/rates-mobile.svg" class="solutions-image md:hidden" />
             </div>
 
-            <div class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
-                <div class="col-span-3 w-full">
-                    <h4
-                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-normal text-center lg:text-left uppercase mb-4 pb-4 tracking-normal leading-tight">
-                        LOW Operational Overhead
-                    </h4>
+            <div class="col-span-3 w-full px-2 lg:px-1">
+                <h4
+                    class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                    Increase Donations Rates
+                </h4>
 
-                    <p
-                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
-                        By streamlining your payment processes with Bitcoin, you can offer more efficient payment
-                        options. Bitcoin and Lightning significantly reduce the complexity associated with managing
-                        traditional payment methods, ultimately cutting down costs and reducing support requirements.
-                    </p>
-                </div>
-
-                <div class="col-span-2 w-full flex justify-center items-center"">
-                    <img src="/assets/document.svg" class="solutions-image" />
-                </div>
+                <p
+                    class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                    Reach more new supporters by offering your donation amounts in more modest amounts. Remove the
+                    friction in the checkout process by displaying a QR code for payments instead of the traditional
+                    credit card experience.
+                </p>
             </div>
-
-            <div class="flex flex-col lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
-                <div class="col-span-2 w-full flex justify-center items-center"">
-                    <img src="/assets/globe.svg" class="solutions-image" />
-                </div>
-
-                <div class="col-span-3 w-full">
-                    <h4
-                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-normal text-center lg:text-left uppercase mb-4 pb-4 tracking-normal leading-tight">
-                        Global Reach
-                    </h4>
-
-                    <p
-                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
-                        Bitcoin's universal nature allows your platform to reach players worldwide without the friction
-                        of currency conversion or dealing with international banking fees. Its global reach makes
-                        Bitcoin an ideal choice for online gambling platforms looking to expand. This capability opens
-                        your gaming platform to new, untapped markets and broadens your potential player base.
-                    </p>
-                </div>
-            </div>
-
-            <div class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
-                <div class="col-span-3 w-full">
-                    <h4
-                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-normal text-center lg:text-left uppercase mb-4 pb-4 tracking-normal leading-tight">
-                        lOW transaction fees
-                    </h4>
-
-                    <p
-                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
-                        Bitcoin payments, especially when leveraging the Bitcoin Lightning Network, are far cheaper than
-                        traditional banking methods, reducing costs for both casinos and their customers. Saving on
-                        costly transaction fees leads to increased profitability for operators and improved player
-                        retention rates.
-                    </p>
-                </div>
-
-                <div class="col-span-2 w-full flex justify-center items-center"">
-                    <img src="/assets/cash.svg" class="solutions-image" />
-                </div>
             </div>
         </section>
 
         <?php include('../includes/faq.php'); ?>
 
-        <?php include('../includes/footer.php'); ?>
+        <?php 
+        $header = "Power your payment with Bitcoin and Bitvora";
+        $text = "Are you ready to lower your transaction costs, broaden your customer base and join the future of online payments? Sign up for a free account today or contact a Bitvora sales representative for more information on how Bitcoin payments can power your freelance business today!";
+        include('../includes/footer.php'); ?>
 
         <script src="/js/main.js"></script>
         <script src="/js/menu.js"></script>
-
 </body>
 
 </html>
