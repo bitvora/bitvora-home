51d1390aafb744098025644875c312ff9c7a376d
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2024-11-29 00:43:06 +0100

    Finish hotels page

---
+++ solutions/online-marketplace.php
@@ -0,0 +1,157 @@
+<html lang="en">
+
+<head>
+    <?php 
+   $pageTitle = "Online Marketplace";
+   $pageURL = "https://signet.bitvora.com/solutions/online-marketplace";
+   $pageImage = "https://signet.bitvora.com/assets/bitvora.png";
+  
+  include('../includes/header.php'); ?>
+</head>
+
+<body>
+    <?php include('../includes/navbar.php'); ?>
+
+    <main class="w-full mb-8 pb-8">
+        <section class="justify-between items-center lg:px-6 mb-6 lg:mb-2 container mx-auto w-full">
+            <div class="relative w-full justify-center text-center flex flex-col-reverse lg:flex-col">
+                <div
+                    class="relative w-full h-full sm:h-[300px] lg:h-[400px] flex justify-end sm:justify-center lg:justify-end lg:py-12 mt-12 py-1 lg:mt-0">
+                    <img src="/assets/online-marketplace.svg"
+                        class="w-full h-full object-contain solutions-header-image hidden md:flex" />
+                    <img src="/assets/online-marketplace-mobile.svg" class="w-full h-full object-fill md:hidden" />
+                </div>
+
+                <div id="hero-section"
+                    class="w-full px-6 md:w-9/12 lg:w-10/12 xl:w-9/12 2xl:w-8/12 relative mx-auto mt-[150px] sm:mt-[120px] md:mt-[140px] lg:mt-2 xl:mt-2 2xl:mt-2 pt-2 sm:pt-[40px] md:pt-2 lg:pt-2 xl:pt-2 2xl:pt-2 justify-center text-center">
+                    <h1
+                        class="text-white text-4xl mx-auto justify-center z-[1] sm:text-4xl md:text-3xl lg:text-4xl 2xl:text-5xl uppercase mb-4 pb-4 tracking-normal leading-tight text-center font-medium lg:w-2/3">
+                        Bitcoin Payments for
+                        Online Marketplaces
+                    </h1>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center sm:font-medium justify-center">
+                        Every year, more and more online marketplaces are choosing to integrate Bitcoin and Lightning
+                        Network payments. Allowing instant, cheap, irreversible Bitcoin payments is bringing a whole new
+                        set of advantages to online marketplaces who wish to add Bitcoin as an additional form of
+                        payment alongside credit cards and PayPal, or completely replace their traditional payment
+                        flows.
+                    </p>
+
+                    <a href="https://console.signet.bitvora.com/auth/signup"
+                        class="bg-primary text-white font-bold px-2 py-3 lg:py-4 rounded-md text-[14px] lg:text-[17px] mt-5 hover:bg-darkBg w-full md:w-[200px] lg:w-[220px] flex text-center justify-center mx-auto">
+                        Get Started Today</a>
+                </div>
+            </div>
+        </section>
+
+        <section
+            class="w-full px-6 py-5 container mx-auto mt-[90px] sm:mt-[80px] md:-mt-[45px] lg:mt-[85px] xl:mt-[85px] 2xl:mt-[85px] pt-[15px] sm:pt-[5px] md:pt-[85px] lg:pt-[85px] xl:pt-[85px] 2xl:pt-[85px] mb-8 pb-8">
+            <h4
+                class="text-white text-2xl sm:text-2xl 2xl:text-3xl font-normal uppercase mb-12 sm:mb-6 md:mb-8 pb-12 sm:pb-6 md:pb-8 tracking-normal leading-tight text-center justify-center md:w-8/12 lg:w-6/12 mx-auto">
+                How Bitcoin Brings New Opportunities for Online Marketplace
+            </h4>
+
+            <div
+                class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12  mb-6 pb-6">
+                <div class="col-span-3 w-full px-2 lg:px-1">
+                    <h4
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Micropayments
+                    </h4>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                        Micropayments aren’t possible on traditional payment processors because of the high base fee.
+                        The bitcoin lightning network allows you to send payments less than $0.01, enabling new
+                        innovative pay per use business models.
+                    </p>
+                </div>
+
+                <div class="col-span-2 w-full flex justify-center items-center mb-2 pb-2 lg:mb-0 lg:pb-0">
+                    <img src="/assets/micropayment.svg" class="solutions-image hidden md:flex" />
+                    <img src="/assets/micropayment-mobile.svg" class="solutions-image md:hidden" />
+                </div>
+            </div>
+
+            <div
+                class="flex flex-col lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12 lg:mt-12 lg:pt-12 mb-6 pb-6">
+                <div class="col-span-2 w-full flex justify-center items-center mb-2 pb-2 lg:mb-0 lg:pb-0">
+                    <img src=" /assets/lock.svg" class="solutions-image hidden md:flex" />
+                    <img src=" /assets/security-mobile.svg" class="solutions-image md:hidden" />
+                </div>
+
+                <div class="col-span-3 w-full px-2 lg:px-1">
+                    <h4
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Improved Security and Fraud Prevention
+                    </h4>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                        Unlike credit cards which are rife with fraud from stolen numbers and carding techniques,
+                        payments made with bitcoin can only be made by the bearer and are irreversible. Say goodbye to
+                        chargebacks and dispute fees with instant settlement, only available with Bitcoin and the
+                        Lightning network.
+                    </p>
+                </div>
+            </div>
+
+            <div
+                class="flex flex-col-reverse lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12 mb-6 pb-6">
+                <div class="col-span-3 w-full px-2 lg:px-1">
+                    <h4
+                        class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                        Reach New Audiences
+                    </h4>
+
+                    <p
+                        class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                        Bitcoin advocates enthusiastically support charities that accept bitcoin as a form of payment.
+                        Enjoy the benefits of free marketing for your charity or NGO simply by accepting a superior form
+                        of payment. Get listed on many popular websites that showcase organizations that are joining the
+                        bitcoin ecosystem.
+                    </p>
+                </div>
+
+                <div class="col-span-2 w-full flex justify-center items-center">
+                    <img src=" /assets/hand-shake.svg" class="solutions-image hidden md:flex" />
+                    <img src="/assets/hand-shake-mobile.svg" class="solutions-image md:hidden" />
+                </div>
+            </div>
+
+            <div class="flex flex-col lg:grid lg:grid-cols-5 w-full lg:w-11/12 mx-auto lg:mb-12 lg:pb-12">
+                <div class="col-span-2 w-full flex justify-center items-center"">
+                    <img src=" /assets/rates-left.svg" class="solutions-image hidden md:flex" />
+                <img src="/assets/rates-mobile.svg" class="solutions-image md:hidden" />
+            </div>
+
+            <div class="col-span-3 w-full px-2 lg:px-1">
+                <h4
+                    class="text-white text-lg sm:text-lg xl:text-2xl 2xl:text-3xl font-semibold lg:font-normal text-center lg:text-left lg:uppercase mb-2 pb-2 tracking-normal leading-tight">
+                    Increase Donations Rates
+                </h4>
+
+                <p
+                    class="text-light font-medium mb-6 pb-6 text-[14px] sm:text-[14px] lg:text-[16px] 2xl:text-[24px] text-center lg:text-left">
+                    Reach more new supporters by offering your donation amounts in more modest amounts. Remove the
+                    friction in the checkout process by displaying a QR code for payments instead of the traditional
+                    credit card experience.
+                </p>
+            </div>
+            </div>
+        </section>
+
+        <?php include('../includes/faq.php'); ?>
+
+        <?php 
+        $header = "Power your payment with Bitcoin and Bitvora";
+        $text = "Are you ready to lower your transaction costs, broaden your customer base and join the future of online payments? Sign up for a free account today or contact a Bitvora sales representative for more information on how Bitcoin payments can power your freelance business today!";
+        include('../includes/footer.php'); ?>
+
+        <script src="/js/main.js"></script>
+        <script src="/js/menu.js"></script>
+</body>
+
+</html>
